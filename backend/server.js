6f5174685cfc2ad0aa92cbb9bd00c5bf88ab8570
techwedfund <144350510+techwedfund@users.noreply.github.com>
2023-12-23 08:21:48 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import router from "./routes/router.js";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express()
 const port = process.env.PORT || 3001;
@@ -15,8 +18,16 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(router)
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, "build", "./build/static/index.html"))
+})
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`)
